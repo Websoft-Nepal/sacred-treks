9d3396f40197b3fde73e9b9da09752da11ddb039
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-03-06 14:09:31 +0545

    blog contact socialmedia model, migration added

--- routes/admin.php
+++ routes/admin.php
@@ -6,5 +6,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('admin')->name("admin.")->group(function () {
     Route::get('/', [HomeController::class, 'index']);
-    Route::get('contact',[ContactUsController::class,'index']);
+
+    //ContactUs Route
+    Route::prefix('contactus')->name('contactus.')->group(function (){
+        Route::get('contact',[ContactUsController::class,'index'])->name('index');
+    });
 });
