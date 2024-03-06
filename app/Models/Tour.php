9d16301e4445f357b75278181ec0d5e0e89c2d99
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-03-06 14:28:10 +0545

    tour, trekking controller, model  added

--- app/Models/Tour.php
+++ app/Models/Tour.php
@@ -5,8 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class ContactUs extends Model
+class Tour extends Model
 {
     use HasFactory;
-    protected $fillable = ['name','email','number','message','map'];
 }
