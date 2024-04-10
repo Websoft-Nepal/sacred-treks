2a94333d6790400f137fa4eb09b44a3964bc6f7e
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-04-10 14:08:12 +0545

    footer change

--- app/Http/Controllers/Api/CompanyInfoController.php
+++ app/Http/Controllers/Api/CompanyInfoController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Api\BaseController;
 use App\Models\contact;
+use App\Models\HomePage;
 use App\Models\SocialMedia;
 use Illuminate\Http\Request;
 use Throwable;
@@ -14,9 +15,11 @@ class CompanyInfoController extends BaseController
         try{
             $contact = contact::first();
             $social = SocialMedia::first();
+            $homepage = HomePage::first();
             $data = [
                 'contact' => $contact,
                 'social' => $social,
+                'footer' => $homepage['footer'],
             ];
             return $this->SendResponse($data,"Company data fetched successfully",200);
         }catch(Throwable $th){
