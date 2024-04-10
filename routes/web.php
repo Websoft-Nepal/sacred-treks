60fef910a90b5c50b2d3efde0e9ddbde3c4208e6
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-04-10 14:00:13 +0545

    some changes

--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,8 @@ use Illuminate\Support\Facades\Auth;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/admin/table',[HomeController::class,'table'])->name('admin.table');
+
 Route::get('/admin/profile',[HomeController::class,'profile'])->name('admin.profile');
 Route::put('/admin/profile-update/{id}',[HomeController::class,'updateProfile'])->name('admin.update-profile');
 Route::put('/admin/password-update/{id}',[HomeController::class,'updatePassword'])->name('admin.update-password');
@@ -27,6 +24,9 @@ Route::put('/admin/password-update/{id}',[HomeController::class,'updatePassword'
 // Admin routes
 
 Auth::routes();
+Route::get('/', function () {
+    return view('welcome');
+});
 include __DIR__ . '/admin.php';
 
 
