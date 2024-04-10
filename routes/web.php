43107ea5c032805f2ca94ef947c51beab46e2302
Sagar Gurung <sgrgug@gmail.com>
2024-04-10 14:00:40 +0545

    Af

--- routes/web.php
+++ routes/web.php
@@ -19,14 +19,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/admin/table',[HomeController::class,'table'])->name('admin.table');
-Route::get('/admin/profile',[HomeController::class,'profile'])->name('admin.profile');
-Route::put('/admin/profile-update/{id}',[HomeController::class,'updateProfile'])->name('admin.update-profile');
-Route::put('/admin/password-update/{id}',[HomeController::class,'updatePassword'])->name('admin.update-password');
+Route::get('/admin/table', [HomeController::class, 'table'])->name('admin.table');
+Route::get('/admin/profile', [HomeController::class, 'profile'])->name('admin.profile');
+Route::put('/admin/profile-update/{id}', [HomeController::class, 'updateProfile'])->name('admin.update-profile');
+Route::put('/admin/password-update/{id}', [HomeController::class, 'updatePassword'])->name('admin.update-password');
 
 // Admin routes
 
-Auth::routes();
+Auth::routes(['register' => false]);
 include __DIR__ . '/admin.php';
 
 
